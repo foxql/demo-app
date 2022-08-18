1f24d6274a1b37d7265554ca83270e0ec3dbbf62
boraozer <nagayapim@gmail.com>
2022-08-18 03:26:52 +0300

    realtime chat app is ready

---
+++ src/routes/+page.svelte
@@ -0,0 +1,93 @@
+<h2>FoxQL - Simple Chat</h2>
+
+<script>
+    import foxql from "@foxql/foxql-peer";
+    import { onMount } from 'svelte';
+    import { messages } from '$lib/store.js';
+
+    let inputValue = ''
+
+    const node = new foxql({
+        maxNodeCount: 50, // max connection limit 
+        maxCandidateCallTime: 750, // how long to wait for a response from a candidate node
+        powPoolingTime: 350,
+        bridgeServer: {
+            host: "https://foxql-bridge.herokuapp.com", // which bridge server to use
+        },
+        dappAlias: 'demo-chat-app'
+    });
+
+    node.setMetaData({
+        name: "test-node",
+        description: "test-desc",
+    });
+
+    node.on("message", async ({ nodeId, message,...webRTCSender }, simulate = false) => {
+        if (simulate) {
+            messages.set([...$messages, {
+                sender: nodeId,
+                message: 'CONNECTED!'
+            }])
+            messages.set([...$messages, {
+                sender: nodeId,
+                message: message
+            }])
+            console.log('This message getting websocket channel.')
+            if(typeof message != 'string') return false;
+            if(typeof message.length > 128) return false;
+
+            return true; // accept webRTC connection.
+        } 
+        console.log('This message getting webrtc channel.')
+        // webRTC message.
+        messages.set([...$messages, {
+            sender: webRTCSender.sender.nodeId,
+            message: message
+        }])
+        
+        // if u reply? use this.reply()
+    });
+
+    async function handleSend()
+    {
+        messages.set([...$messages, {
+            sender: 'ME',
+            message: inputValue
+        }])
+        node.ask({
+            transportPackage: {
+                p2pChannelName: "message",
+                message: inputValue,
+            },
+            livingTime: 1000,
+            stickyNode: true,
+            localWork: false
+        });
+
+        inputValue = ''
+    }
+
+    onMount(()=> {
+        node.start();
+    })
+</script>
+
+<ul>
+    {#if $messages.length <= 0}
+        <h3>Waiting new message :)</h3>
+    {/if}
+    {#each $messages as {sender, message}}
+        <li><b>{sender}</b>: {message}</li>
+    {/each}
+</ul>
+
+<input type = "text" placeholder = "your message" bind:value="{inputValue}">
+<button on:click="{handleSend}">Send</button>
+<style>
+    ul {
+        border: 1px dashed #333;
+    }
+    input {
+        width: 40%;
+    }
+</style>
